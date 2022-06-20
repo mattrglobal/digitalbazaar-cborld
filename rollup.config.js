bc7b9674d2a8585e1b78dcb28659bb2e666c3248
Marcus Ball <marcus.ball@mattr.global>
2022-06-20 13:41:44 +1200

    Added comment on reasonings why base58 was the only package resolved using nodeResolve

--- rollup.config.js
+++ rollup.config.js
@@ -7,5 +7,8 @@ export default {
     format: 'cjs',
     sourcemap: true
   },
+  // Using nodeResolve to bundle the base58-universal library within the output dist/main.js
+  // This is required as digitalbazaar has migrated this library to be an ES module, 
+  // making it incompatible with @mattrglobal/opencore
   plugins: [nodeResolve({ resolveOnly: ["base58-universal"] })]
 };
